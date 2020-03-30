67adaaefba4913936881c2b463ac9df6058d0aa8
rmricci121 <rmricci121@gmail.com>
2020-03-30 07:12:41 -0400

    Added sign-up form. made adjustments to names of controllers

--- app/controllers/hike_controller.rb
+++
@@ -1,5 +0,0 @@
-class HikeController < ApplicationController
-
-
-
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
 
+    get "/signup" do 
+        
+        erb :"user/signup"
+    end
 
 
 end
