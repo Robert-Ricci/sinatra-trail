da08c0ee5521438805fd1987756d2c6ee4618e6c
rmricci121 <rmricci121@gmail.com>
2020-03-30 07:32:25 -0400

    wrote my sign up post route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,14 @@ class UsersController < ApplicationController
 
     post "/signup" do 
         user = User.create(params)
-    
+        if user.valid?
+            session[:user_id] = user.id 
+            redirect to "user/#{user.id}"
+        else
+            redirect to "/signup"
+        end
     end
+
     get "/login" do
         erb :"user/login"
     end
